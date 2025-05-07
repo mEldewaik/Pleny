c8a4593bc942e0a6b0b77cbf0a2b240eafa348d1
Mohamed Ezzat <mohamed.ezzat@kickstartinteractive.net>
2025-05-07 23:07:55 +0300

    Initial Commit

---
+++ PlenyApp/PlenyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyAppApp.swift
+//  PlenyApp
+//
+//  Created by Mohamed Eldewaik on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlenyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlenyAppTests/PlenyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyAppTests.swift
+//  PlenyAppTests
+//
+//  Created by Mohamed Eldewaik on 07/05/2025.
+//
+
+import Testing
+@testable import PlenyApp
+
+struct PlenyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
